f9da30be77a2c87308a1d30047ea44e9d0f4b81e
CoderXDY <444875571@qq.com>
2017-02-18 14:48:07 +0800

    init

---
+++ mathematics.js
@@ -0,0 +1,36 @@
+/*
+ a javascript library about mathematics, which used to machine learning
+ see also  numericJs
+*/
+
+var Mathematics = (function(){
+
+	var Set = {
+
+		union : function(x, y){},
+
+		intersection : function(x, y){},
+
+		differ : function(x, y){}
+
+	};
+
+	var Matrix = {
+
+		dim : function(matrix){
+			
+			dims = [];
+			if(matrix === )
+			while()
+		},
+
+	}
+
+	return{
+
+		Set : Set,
+		Matrix : Matrix
+	};
+
+
+})();
